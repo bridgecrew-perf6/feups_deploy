460e75390f391b014972d9c54adc293c094f2244
Michael Robles <michaeljavier26@hotmail.com>
2022-03-11 10:53:24 -0500

    origin

--- index.js
+++ index.js
@@ -11,13 +11,21 @@ const app = express();
 require('./config/db');
 app.use(cors());
 app.use(express.json());
+app.use(function(req,res,next){
+        res.header('Access-Control-Allow-Origin','*');
+        res.header('Access-Control-Allow-Credentials',true);
+        res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,content-type,application/json')
+    next();
+}
+);
 
 
 app.use('/api/registrados',require('./routes/registrado'));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
-app.use(express.static('cliente'));
+app.use(express.static('cliente'));// Servidor node como estático
 
 app.listen(process.env.PORT || 4000,() => 
 {
