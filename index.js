9f998726c2ea58b219e56fefce847ae11d05151e
Michael Robles <michaeljavier26@hotmail.com>
2022-03-11 10:06:50 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const bodyparser = require('body-parser');
+const cors = require("cors");
+
+require('dotenv').config({path: 'variables.env'});
+//Creación de servidor
+
+
+const app = express();
+require('./config/db');
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/api/registrados',require('./routes/registrado'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.use(express.static('cliente'));
+
+app.listen(process.env.PORT || 4000,() => 
+{
+    console.log("El servidor está corriendo")
+})
+
+module.exports = app;
